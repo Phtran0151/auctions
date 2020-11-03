f9f3a89e7ef3899a8992935afc46e0290b4c914d
Phtran0151 <phuong.tran@tomosia.com>
2020-11-03 21:20:02 +0700

    aaddd

--- routes/users/get.dashboard.js
+++ routes/users/get.dashboard.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 /* GET Dashboard page of users. */
 router.get('/', function(req, res, next) {
-  // let data = {
-  //   id: '',
-  //   username: []
-  // }
-  // data.id = req.query._id;
-  // console.log(data.id)
-  // data.username = data.username.concat(req.query.username)
+  let data = {
+    id: '',
+    username: []
+  }
+  data.id = req.query._id;
+  console.log(data.id)
+  data.username = data.username.concat(req.query.username)
   res.render("users/dashboard?=" + data._id);
 });
 
