8301d65e40d5e9017e69db8effc97acdf95fde30
Phtran0151 <phuong.tran@tomosia.com>
2020-11-07 17:01:08 +0700

    Custom all page

--- routes/admin/get.dashboard.js
+++ routes/admin/get.dashboard.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const mongo = require("mongodb");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin/dashboard');
+  mongo.connect(require('../routes/path.mongodb'), (err, db) => {
+    db.collections('users').count((err, user) => {
+      db.close();
+      res.render("admin/dashboard", { account: user })
+    })
+  })
 });
 
 module.exports = router;
