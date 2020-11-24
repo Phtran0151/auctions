d43f84edb2ab8bad0e5fc55dfc1b99bbaa340368
Phtran0151 <phuong.tran@tomosia.com>
2020-11-24 13:59:37 +0700

    Done Delete Products

--- routes/products/remove.products.js
+++ routes/products/remove.products.js
@@ -7,24 +7,22 @@ const fs = require('fs')
 
 /* GET remove products. */
 router.get('/:id', (req, res, next) => {
-  res.send(`/productsAll/${req.query._id}`)
-  console.log(req.query._id)
-  // mongo.connect(mongoPath, (err,db) => {
-  //   db.collection('products').findOne({_id: ObjectId(req.query._id)}, (err, data) => {
-  //     db.collection('products').deleteOne({_id: ObjectId(req.query._id)}, (err, result) => {
-  //       if(!err){
-  //         fs.unlink(`public/${data.image.split("./").join("")}`, (err) => {
-  //           if(!err){
-  //             res.send(`/productsAll/${req.query._id}`)
-  //           }
-  //         })
-  //       } else {
-  //         res.render("error")
-  //       }
-  //     })
-  //   })
-  //   db.close();
-  // })
+  mongo.connect(mongoPath, (err,db) => {
+    db.collection('products').findOne({_id: ObjectId(req.query._id)}, (err, data) => {
+      db.collection('products').deleteOne({_id: ObjectId(req.query._id)}, (err, result) => {
+        db.close();
+        if(!err){
+          fs.unlink(`public/${data.image.split("./").join("")}`, (err) => {
+            if(!err){
+              res.send(`/productsAll/${req.query._id}`)
+            }
+          })
+        } else {
+          res.render("error")
+        }
+      })
+    })
+  })
 });
 
 module.exports = router;
