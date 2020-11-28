477e3925944936137868d05f2644e9c849438636
Phtran0151 <phuong.tran@tomosia.com>
2020-11-28 16:01:19 +0700

    hello

--- public/shop/assets/js/count-down.js
+++ public/shop/assets/js/count-down.js
@@ -6,14 +6,9 @@ $('.bid-timer').each(function() {
     let set_date = event.strftime('%D : %H : %M : %S')
     $(this).html(set_date)
     // If every element expired times
-    if (set_date === '00 00 : 00 : 00') {
-      $('.mess').each(function(){
-        $(this).html('EXPIRES').css({
-          'color': '#e3aa84',
-          'background': '#84e3e3',
-          'border': '1px solid #425447',
-          'border-radius': '5px'
-        })
+    if (set_date === '00 : 00 : 00 : 00') {
+      $(this).html('EXPIRES').css({
+        'color': '#e60000'
       })
     }
   });
