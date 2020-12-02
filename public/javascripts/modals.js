8889c12e9bccab2c8a462e6555f576028cb5794e
Phtran0151 <phuong.tran@tomosia.com>
2020-12-02 21:52:03 +0700

    add cart

--- public/javascripts/modals.js
+++ public/javascripts/modals.js
@@ -1,9 +1,18 @@
-$('.bid_auction').on('click', function() {
-  $('.auction-item').each(function(){
-    console.log('msg')
+$('.row-products').each(function(){
+  let name = $(this).find('.name-product').html()
+  let id = $(this).find('.auction-id').html()
+  let price = $(this).find('.bid-price').html()
+  let times = $(this).find('.bid-timer').html()
+  let image = $(this).find('.prod2').attr('src')
+  $(this).find('.bid_auction').one('click', function() {
+    //<!--------------- Change value of every product ---------- >
+    console.log(price)
+    $('.paypal').find('.name').html(name)
+    $('.paypal').find('.price').html(price)
+    $('.paypal').find('.times').html(times)
+    $('.paypal').find('.paypal__subheader-wrapper').attr('src', image)
+    $('.paypal').slideToggle("slow")
   })
-  $('.paypal').slideToggle("slow")
-  // $("body").toggleClass("blur-body").not('.paypal')
 })
 
 // Closed modals
