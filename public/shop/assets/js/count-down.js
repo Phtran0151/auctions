01d6372c3cc5a79ddb2cbfa2aaf789e165f09024
Phtran0151 <phuong.tran@tomosia.com>
2020-12-03 17:52:13 +0700

    Custom countdown

--- public/shop/assets/js/count-down.js
+++ public/shop/assets/js/count-down.js
@@ -1,15 +1,23 @@
 /*------------- Count dowm times set auction --------------*/
-$('.bid-timer').each(function() {
-  let finalDate = $(this).data('countdown');
-  // Countdown every element
-  $(this).countdown(finalDate, function(event) {
+
+$('.row-products').each(function() {
+  let times_count = $(this).find('.bid-timer')
+  let btn = $(this).find('.bid_auction')
+  let mess = $(this).find('.mess')
+  times_count.countdown(times_count.data('countdown'), function(event) {
     let set_date = event.strftime('%D : %H : %M : %S')
     $(this).html(set_date)
     // If every element expired times
     if (set_date === '00 : 00 : 00 : 00') {
-      $(this).html('EXPIRES').css({
-        'color': '#e60000'
-      })
+      mess.html("The auction time has ended")
+      btn.disable();
+      $(this).text('EXPIRES').css('color', '#e60000')
     }
   });
-});
+})
+
+$.prototype.disable = function () {
+  $.each(this, function (index, el) {
+    $(el).attr('disabled', true).css('background', 'grey')
+  });
+}
