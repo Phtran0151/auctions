fe3319d7234620f98718de30795f07f7bb446635
Phtran0151 <phuong.tran@tomosia.com>
2020-12-06 17:40:09 +0700

    Done countdown modals

--- public/javascripts/modals.js
+++ public/javascripts/modals.js
@@ -1,42 +1,48 @@
-$('.row-products').each(function(){
-  let name = $(this).find('.name-product').html()
-  let id = $(this).find('.auction-id').html()
-  let price = $(this).find('.bid-price').html()
-  let image = $(this).find('.prod2').attr('src')
-  let times_count = $(this).find('.bid-timer')
-  let mess = $(this).find('.mess')
-  let btn = $(this).find('.bid_auction')
+$(document).ready(function() {
+  $('.row-products').each(function(){
+    let name = $(this).find('.name-product').html()
+    let id = $(this).find('.auction-id').html()
+    let price = $(this).find('.bid-price').html()
+    let image = $(this).find('.prod2').attr('src')
+    let times_count = $(this).find('.bid-timer')
+    let mess = $(this).find('.mess')
+    let btn = $(this).find('.bid_auction')
 
-  /*------------- Count dowm times set auction --------------*/
-  times_count.countdown(times_count.data('countdown'), function(event) {
-    let set_date = event.strftime('%D : %H : %M : %S')
-    $(this).html(set_date)
-    // If every element expired times
-    if (set_date === '00 : 00 : 00 : 00') {
-      mess.html("The auction time has ended")
-      btn.disable();
-      $(this).text('EXPIRES').css('color', '#e60000')
-    }
-  });
+    /*------------- Count dowm times set auction --------------*/
+    times_count.countdown(times_count.data('countdown'), function(event) {
+      let set_date = event.strftime('%D : %H : %M : %S')
+      $(this).html(set_date)
+      // If every element expired times
+      if (set_date === '00 : 00 : 00 : 00') {
+        mess.html("The auction time has ended")
+        btn.disable();
+        $(this).text('EXPIRES').css('color', '#e60000')
+      }
+    });
 
-  // <!--------------- Change value of every product ----------------- >
-  $(this).find('.bid_auction').on('click', function() {
-    $('.times').clone().append($(times_count).appendTo('.times'))
-    $('.paypal').find('.name').html(name)
-    $('.paypal').find('.price').html(price)
-    $('.paypal').find('.paypal__subheader-wrapper').attr('src', image)
-    $('.paypal').slideToggle("slow")
+    // <!--------------- Change value of every product ----------------- >
+    $(this).find('.bid_auction').on('click', function() {
+      times_count.clone().appendTo('.times').countdown(times_count.data('countdown'), function(event) {
+        let set_date = event.strftime('%D : %H : %M : %S')
+        $(this).html(set_date)
+      })
+      $('.paypal').find('.name').html(name)
+      $('.paypal').find('.price').html(price)
+      $('.paypal').find('.paypal__subheader-wrapper').attr('src', image)
+      $('.paypal').slideToggle("slow")
+    })
+    // Closed modals
+    $('.closed-button').on('click', function() {
+      $('.paypal').hide()
+      $('.paypal').find('.bid-timer').remove()
+    })
   })
-})
 
-// Closed modals
-$('.closed-button').on('click', function() {
-  $('.paypal').hide()
-})
 
-// Disable button bid
-$.prototype.disable = function () {
-  $.each(this, function (index, el) {
-    $(el).attr('disabled', true).css('background', 'grey')
-  });
-}
+  // Disable button bid
+  $.prototype.disable = function () {
+    $.each(this, function (index, el) {
+      $(el).attr('disabled', true).css('background', 'grey')
+    });
+  }
+})
