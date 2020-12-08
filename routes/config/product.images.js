978cbbade04be954d3820a7a9282f9cf2959f924
Phtran0151 <phuong.tran@tomosia.com>
2020-12-08 17:08:53 +0700

    Using dashboard ejs

--- routes/config/product.images.js
+++ routes/config/product.images.js
@@ -1,6 +1,6 @@
 var index = 0;
 module.exports = {
-  destination: `public/uploads/`,
+  destination: `public/uploads/products`,
   filename: function ( req, file, cb ) {
     let typeFile = file.originalname.split(".")[file.originalname.split(".").length-1];
     cb( null, `${index++}_${req.body['post_date']}.jpg`);
