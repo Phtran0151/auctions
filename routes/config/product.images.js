857a882cdfaaa9526a3ba38e2ca0989c2020541b
Phtran0151 <phuong.tran@tomosia.com>
2020-12-11 23:53:19 +0700

    Change avatar

--- routes/config/product.images.js
+++ routes/config/product.images.js
@@ -3,6 +3,6 @@ module.exports = {
   destination: `public/uploads/products`,
   filename: function ( req, file, cb ) {
     let typeFile = file.originalname.split(".")[file.originalname.split(".").length-1];
-    cb( null, `${index++}_${req.body['post_date']}.jpg`);
+    cb( null, `${index++}_${req.body['images_pro']}.jpg`);
   }
 }
--- routes/users/put.profile.js
+++ routes/users/put.profile.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const mongo = require("mongodb");
-const path = require("../path.mongodb");
+const pathMongo = require("../path.mongodb");
+const ObjectId = require('mongodb').ObjectId;
 const multer = require("multer")
-const produceImage = require('../config/avatar.images');
-const storage = multer.diskStorage(produceImage)
+const avatarImage = require('../config/avatar.images');
+const storage = multer.diskStorage(avatarImage)
 const upload = multer({ storage: storage });
 
 /* UPDATE account POST method. */
-router.post('/', (req, res, next) => {
-  console.log(req.body)
-  // mongo.connect(path, (err, db) => {
-  //   db.collection("auctions").insertOne(req.body, (err, result) => {
-  //     db.close();
-  //     if (!err) {
-
+router.post('/', upload.single('avatar'), (req, res, next) => {
+  let session = req.session.user
+  let id = session.id
+  let account = req.body
+  let profile = {
+    avatar: account.avatar,
+    gender: account.gender,
+    birthday: account.birthday,
+  }
+  console.log(req.files)
+  // mongo.connect(pathMongo, (err, db) => {
+  //   profile.avatar = req.file.path.split("public").join(".")
+  //   for (var i = 0; i < Object.keys(profile).length; i++) {
+  //     if(!(isNaN(Object.values(profile)[i])) && Object.keys(profile)[i] !== "tel"){
+  //       profile[`${Object.keys(profile)[i]}`] = Number(Object.values(profile)[i]);
   //     }
+  //   }
+  //   db.collection('users').findOne({_id: ObjectId(id)}, (err, data) => {
+  //     db.collection('users').update({_id: ObjectId(id)}, { $set: profile }, (err, result) => {
+  //       db.close();
+  //       if (!err) {
+  //         res.render('users/profile')
+  //       } else {
+  //         res.redirect(req.get('referer'))
+  //       }
+  //     })
   //   })
   // })
 });
